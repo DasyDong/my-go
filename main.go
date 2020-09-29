8fe83b3fa4fe85d5b7ebe7fe143c8d79117da840
dongshu <dongshu@qutoutiao.net>
2020-09-29 15:10:54 +0800

    a

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
+/* Go quine */
 package main
 
 import "fmt"
 
 func main() {
-	var a = [...]int{1, 2, 3} // a 是一个数组
-	var b = &a                // b 是指向数组的指针
-
-	fmt.Println(a[0], a[1])   // 打印数组的前2个元素
-	fmt.Println(b[0], b[1])   // 通过数组指针访问数组元素的方式和数组类似
-
-	for i, v := range b {     // 通过数组指针迭代数组的元素
-		fmt.Println(i, v)
-	}
-
+	fmt.Printf("%s%c%s%c\n", q, 0x60, q, 0x60)
 }
+
+var q = `/* Go quine */
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("%s%c%s%c\n", q, 0x60, q, 0x60)
+}
+
+var q = `
